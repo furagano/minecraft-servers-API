75b43731490c59b170b040ba41a31bc36c7454d7
furagano <cee2013@yandex.ru>
2022-10-08 16:10:23 +0500

    input debug

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,73 @@
 <template>
-    <div></div>
+<div class="app">
+    <form @submit.prevent class="container">
+        
+        <div class="container_input">
+            <input 
+            class="input" 
+            @input="ip = $event.target.value"
+            placeholder="Напишите ip" 
+            :value="ip" 
+            type="text">
+        </div>
+        <button class="btn" @click="findServer">Найти</button>
+    </form>
+    <ServerList :servers="servers"/>
+       
+</div>
+
 </template>
 
 <script>
-
+import ServerList from "@/components/ServerList.vue";
+export default{
+    
+    components: {
+        ServerList
+    },
+    data() {
+        ip: ' '
+    },
+    methods: {
+        findServer(){
+            const newServer = {
+                id: Date.now(),
+                ip: this.ip
+            }
+            this.ServerList.servers.push(newServer);
+        }
+    },
+}
 </script>
 
 <style>
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    .app{
+        padding: 20px;
+    }
+    .container{
+        padding-left: 40%;
+        display: inline-block;
+    }
+    .container_input{
+        float: left;
+    }
+    .input{
+        width: 100%;
+        border: 1px solid teal;
+        padding: 10px 15px;
+
+    }
+    .btn{
+        border: 1px solid teal;
+        padding: 10px 15px;
+        background: none;
+        color: teal;
+    }
+    
 
 </style>
---
+++ src/components/ServerList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="server" v-for="server in servers">
+        <div><strong>Название:</strong> {{server.name}} </div>
+        <div><strong>Онлайн:</strong> {{server.online}} </div>
+        <div><strong>IP:</strong> {{server.ip}} </div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+          servers:[
+            {id:1, name: 'hypixel', online: '50', ip: '214037598623875'},
+            {id:2, name: 'hypixel 2', online: '50', ip: '214037598623875'},
+        ]  
+        }
+        
+    },
+    methods(){
+        
+    }
+}
+</script>
+<style scoped>
+    .server{
+        margin-top: 15px;
+        background-color: rgb(92, 241, 92);
+        padding: 15px;
+        border: 2px solid saddlebrown;
+        border-radius: 12px;
+        box-shadow:inset 0 -6em 6em rgba(0, 0, 0, 0.2),
+        0.3em 1em 1em rgba(0, 0, 0, 0.3);
+    }
+</style>
